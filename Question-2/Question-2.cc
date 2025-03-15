796cf5caa56fe115d4895b7866db330d7e4f0d3d
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2025-03-15 22:15:50 +1100

    Completed Q2

--- Question-2/Question-2.cc
+++ Question-2/Question-2.cc
@@ -7,48 +7,49 @@
 #include <vector>
 #include <thread>
 #include <iostream>
+#include <mutex>
 
 int total = 0;
 
-class Wallet
-{
+class Wallet {
     int mMoney;
+    std::mutex mMutex;
 public:
-    Wallet() :mMoney(0) {}
+    Wallet() : mMoney(0) {}
+
     int getMoney() { return mMoney; }
-    void addMoney(int money)
-    {
-        for (int i = 0; i < money; ++i)
-        {
+
+    void addMoney(int money) {
+        std::lock_guard<std::mutex> lock(mMutex);
+        for (int i = 0; i < money; ++i) {
             mMoney++;
         }
     }
 };
 
-
-int fillWalletWithMoney()
-{
+int fillWalletWithMoney() {
     Wallet walletObject;
     std::vector<std::thread> threads;
+
     for (int i = 0; i < 5; ++i) {
         threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
     }
-    for (int i = 0; i < threads.size(); i++)
-    {
-        threads.at(i).join();
+
+    for (size_t i = 0; i < threads.size(); i++) {
+        threads[i].join();
     }
+
     return walletObject.getMoney();
 }
-int main()
-{
+
+int main() {
     int val = 0;
-    for (int k = 0; k < 1000; k++)
-    {
-        if ((val = fillWalletWithMoney()) != 5000)
-        {
+    for (int k = 0; k < 1000; k++) {
+        if ((val = fillWalletWithMoney()) != 5000) {
             std::cout << "Error at count = " << k << " Money in Wallet = " << val << std::endl;
             return EXIT_FAILURE;
         }
     }
+
     return 0;
 }
