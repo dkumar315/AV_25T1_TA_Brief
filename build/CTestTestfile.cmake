df07bff3e6c02343d0c94ad72ccd06cf896cdeb1
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2025-03-15 22:02:18 +1100

    Completed Q1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/devaansh/AV_25T1_TA_Brief
+# Build directory: /home/devaansh/AV_25T1_TA_Brief/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(question-1 "Question-1-out")
+set_tests_properties(question-1 PROPERTIES  _BACKTRACE_TRIPLES "/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;13;add_test;/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;0;")
+add_test(question-2 "Question-2-out")
+set_tests_properties(question-2 PROPERTIES  _BACKTRACE_TRIPLES "/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;13;add_test;/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;0;")
+add_test(question-3 "Question-3-out")
+set_tests_properties(question-3 PROPERTIES  _BACKTRACE_TRIPLES "/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;13;add_test;/home/devaansh/AV_25T1_TA_Brief/CMakeLists.txt;0;")
